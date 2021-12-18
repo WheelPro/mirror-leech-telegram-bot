f5224ced55bd312a0ab28fae8557e1b7a323e39b
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2021-12-18 07:56:47 +0530

    Update rssfeeds.py

--- bot/modules/rssfeeds.py
+++ bot/modules/rssfeeds.py
@@ -113,8 +113,8 @@ def rss_monitor(context):
                     feed_urls.insert(0, rss_d.entries[feed_count]['link'])
                     feed_count += 1
                 for x in range(len(feed_urls)):
-                    feed_info = f"<b>{feed_titles[x]}</b>\n{feed_urls[x]}"
-                    feed_new = f"/mirror7 `{feed_urls[x]}`"
+                    feed_info = f"<b>{feed_titles[x]}</b>\n<a href="{feed_urls[x]}"><b>⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜</b></a>"
+                    feed_new = f"/mirror7 {feed_urls[x]}"
                     if rss_session is None:
                         context.bot.send_message(CHAT_ID, feed_info, parse_mode='HTMl')
                         context.bot.send_message(CHAT_ID, feed_new, parse_mode='HTMl')
